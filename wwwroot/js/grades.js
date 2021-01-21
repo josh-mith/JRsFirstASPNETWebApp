5716a88728bb7a335f331da5bd463625ef8b559f
josh-mith <53532738+josh-mith@users.noreply.github.com>
2021-01-20 21:44:02 -0700

    Add project files.

---
+++ wwwroot/js/grades.js
@@ -0,0 +1,72 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+
+    //Get grade values
+    var assignments = document.getElementById("assignments").value;
+    var groupProject = document.getElementById("groupProjects").value; 
+    var quizzes = document.getElementById("quizzes").value;
+    var exams = document.getElementById("exams").value;
+    var intex = document.getElementById("intex").value;
+
+    //Grade weights
+    var assignWeight = .5;
+    var groupProjectWeight = .1;
+    var quizWeight = .1;
+    var examWeight = .2;
+    var intexWeight = .1;
+
+    //Calculate grade weights
+    var assignFinal = assignments * assignWeight;
+    var groupFinal = groupProject * groupProjectWeight;
+    var quizFinal = quizzes * quizWeight;
+    var examFinal = exams * examWeight;
+    var intexFinal = intex * intexWeight;
+
+    var finalGrade = 0;
+
+    //Calculate final grade
+    finalGrade = (assignFinal + groupFinal + quizFinal + examFinal + intexFinal);
+
+    //Assign letter grade
+    var letterGrade = "";
+    if (finalGrade >= 94) {
+        letterGrade = "A";
+    }
+    else if (finalGrade >= 90 && finalGrade < 94) {
+        letterGrade = "A-";
+    }
+    else if (finalGrade >= 87 && finalGrade < 90) {
+        letterGrade = "B+";
+    }
+    else if (finalGrade >= 84 && finalGrade < 87) {
+        letterGrade = "B";
+    }
+    else if (finalGrade >= 80 && finalGrade < 84) {
+        letterGrade = "B-";
+    }
+    else if (finalGrade >= 77 && finalGrade < 80) {
+        letterGrade = "C+";
+    }
+    else if (finalGrade >= 74 && finalGrade < 77) {
+        letterGrade = "C";
+    }
+    else if (finalGrade >= 70 && finalGrade < 74) {
+        letterGrade = "C-";
+    }
+    else if (finalGrade >= 67 && finalGrade < 70) {
+        letterGrade = "D+";
+    }
+    else if (finalGrade >= 64 && finalGrade < 67) {
+        letterGrade = "D";
+    }
+    else if (finalGrade >= 60 && finalGrade < 64) {
+        letterGrade = "D-";
+    }
+    else if (finalGrade < 60) {
+        letterGrade = "E";
+    }
+
+    //Display letter grade and final grade to user
+    alert("Letter grade: " + letterGrade + "\nPercentage: " + finalGrade);
+});
